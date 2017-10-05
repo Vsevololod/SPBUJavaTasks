1f0db9534de725cb93f6d937fb2855cef8279848
Vsevolod <vsevolodtachii@gmail.com>
2017-10-05 14:45:31 +0300

    add IEEEMod to task 1

--- src/test/java/EuclidTest.java
+++ src/test/java/EuclidTest.java
@@ -10,19 +10,25 @@ import org.testng.annotations.Test;
 public class EuclidTest {
     @DataProvider
     public static Object[][] createCorrectData() {
-        return new Object[][] {
-                new Object[] {30,36,6},
-                new Object[] {50,25,25}
+        return new Object[][]{
+                new Object[]{30, 36, 6},
+                new Object[]{50, 25, 25}
         };
     }
 
     @Test(dataProvider = "createCorrectData")
-    public static void testSimpleMod(int x1,int x2,int result){
-        Assert.assertEquals(EuclidAlgorithmImpl.gcd(x1,x2,(a, b)-> (int)a%(int)b),result);
+    public static void testSimpleMod(int x1, int x2, int result) {
+        Assert.assertEquals(EuclidAlgorithmImpl.gcd(x1, x2, (a, b) -> (int) a % (int) b), result);
     }
 
     @Test(dataProvider = "createCorrectData")
-    public static void testFloorMod(int x1,int x2,int result){
-        Assert.assertEquals(EuclidAlgorithmImpl.gcd(x1,x2,(a, b)-> java.lang.Math.floorMod((int)a,(int)b)),result);
+    public static void testFloorMod(int x1, int x2, int result) {
+        Assert.assertEquals(EuclidAlgorithmImpl.gcd(x1, x2, (a, b) -> java.lang.Math.floorMod((int) a, (int) b)), result);
+    }
+
+    @Test(dataProvider = "createCorrectData")
+    public static void testIEEEMod(int x1, int x2, int result) {
+        Assert.assertEquals(EuclidAlgorithmImpl.gcd(x1, x2, (a, b) -> (int) java.lang.Math.IEEEremainder((int) a, (int) b)), result);
+
     }
 }
